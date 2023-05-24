d8029f6d51986230a0ae026446873d8626e19f1b
Guilherme Araujo <Guilherme.Nascimento.Stefanini@cnpbrasil.com.br>
2023-05-24 17:57:36 -0300

    feat: init project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/glogal'
 import type { AppProps } from 'next/app'
+import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
+import logoimg from '../assets/logo.svg'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <Container>
+      <Header>
+        <Image src={logoimg} alt=''/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  ) 
 }
