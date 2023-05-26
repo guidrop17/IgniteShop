79b449e4f7b05bcfecf2f4b78742c7c4a3e16192
Guilherme Araujo Nascimento <guilherme.nascimento.stefanini@cnpbrasil.com.br>
2023-05-26 16:07:53 -0300

    CD: Action Deploy

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
 
-const stripeSecretKey = process.env.NEXT_SECRET_STRIPE_API_KEY || '';
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY || '';
 
 export const stripe = new Stripe(stripeSecretKey, {
     apiVersion: "2022-11-15",
